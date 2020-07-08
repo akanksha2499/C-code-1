6e4cf347009c86fb116688e13d1612cd3ff0a957
akanksha2499 <67830410+akanksha2499@users.noreply.github.com>
2020-07-08 15:55:51 +0530

    Created 2nd program of challenge of MyCaptian C++ workshop
    This program calculates the product of the two numbers entered by the user.

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+   {
+   	float n,m;
+   	cout<<"Enter two numbers\n";
+   	cin>>n;
+   	cin>>m;
+   	cout<<"Product = "<<n*m<<endl;
+   	return 0;
+   }
